9b0dc928b72ddf118a745d39707abbd13d9c1fc4
RamiroG8k <ramiro.g8k@gmail.com>
2023-08-26 11:44:59 -0600

    Init configuration for a nextjs frontend base

--- src/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  serverHost: 'http://127.0.0.1',
-  serverPort: '8000'
-};
-
-export default config;
--- src/views/PlayLists/getLists.js
+++
@@ -1,7 +0,0 @@
-import api from '../../api';
-
-export default async function getLists() {
-  const { data } = await api.get('lists');
-
-  return data;
-}
